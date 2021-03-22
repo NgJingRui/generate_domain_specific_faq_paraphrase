0b517332814f84ac22a8ff125e5257cf06e8f602
James <ngjingrui@hotmail.com>
2021-03-22 09:55:15 +0800

    Add a tip for mac users running the python scripts

--- batch_generate_app_interactive.py
+++ batch_generate_app_interactive.py
@@ -14,5 +14,6 @@ keep_top_k_paraphrases = int(input("# of top paraphrases to retain from candidat
 
 assert keep_top_k_paraphrases > 0, "keep_top_k_paraphrases should be positive"
 
-print(f"python3 batch_generate_app.py -i {input_file} -o {output_file} --n_gen {generate_n_paraphrases} --k_top {keep_top_k_paraphrases}")
-os.system(f"python3 batch_generate_app.py -i {input_file} -o {output_file} --n_gen {generate_n_paraphrases} --k_top {keep_top_k_paraphrases}")
+# Mac users may have to update "python" to "python3". Python version used: 3.7.7
+print(f"python batch_generate_app.py -i {input_file} -o {output_file} --n_gen {generate_n_paraphrases} --k_top {keep_top_k_paraphrases}")
+os.system(f"python batch_generate_app.py -i {input_file} -o {output_file} --n_gen {generate_n_paraphrases} --k_top {keep_top_k_paraphrases}")
